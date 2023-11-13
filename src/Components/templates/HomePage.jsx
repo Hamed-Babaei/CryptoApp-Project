00a840d38d0a03804dc864555c947368a07dd2b1
Hamed Babaei <dev.hamedbabaei@gmail.com>
2023-11-14 00:43:21 +0330

    fixed bug

--- src/Components/templates/HomePage.jsx
+++ src/Components/templates/HomePage.jsx
@@ -21,7 +21,7 @@ export default function HomePage() {
         setCoins(json);
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
+        // console.log(error);
       }
     };
     getData();
